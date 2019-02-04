9021c84a8a66a95572527326f51c4a30feb5c3fb
Abdul Rahman Khan <rahman.khan803@gmail.com>
2019-02-04 12:12:27 +0500

    Added card and date list

--- src/App.js
+++ src/App.js
@@ -8,68 +8,94 @@ import ReactList from "react-list";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
+import { Card } from "@material-ui/core";
+import moment from 'moment';
 
 var data = [
   {
-    name: "Badar"
+    name: "Monday"
   },
   {
-    name: "Badar"
+    name: "Tuesday"
   },
   {
-    name: "Badar"
+    name: "Wednesday"
   },
   {
-    name: "Badar"
+    name: "Thursday"
   },
   {
-    name: "Badar"
+    name: "Friday"
   },
   {
-    name: "Badar"
+    name: "Saturday"
   },
   {
-    name: "Badar"
-  },
-  {
-    name: "Badar"
-  },
-  {
-    name: "Badar"
-  },
-  {
-    name: "Badar"
-  },
-  {
-    name: "Badar"
-  },
-  {
-    name: "Badar"
-  },
-  {
-    name: "Badar"
-  },
-  {
-    name: "Badar"
-  },
-  {
-    name: "Badar"
+    name: "Sunday"
   }
 ];
-
+var count = -10;
 class App extends Component {
+  ShowCurrentDate(){
+ 
+    var date = new Date().getDate()+count;
+    var month = new Date().getMonth() ;
+    var year = new Date().getFullYear();
+    return (date + '-' + month + '-' + year);
+   }
+
   render() {
     var items = data.map((item, index) => {
       return (
-        <div key={index}>
+        <div key={index} 
+        style={{ justifyContent: 'center'}}
+        >
           <ListItem
-            style={{ background: "yellow", width: 140, height: 62, margin: 8 }}
+            style={{ background: "yellow", width: 140 , height: 62, margin: 8 }}
           >
             <ListItemText
-              primary={item.name + " " + index}
-              secondary="Jan 9, 2014"
+              primary={item.name+ " "}
             />
+            
           </ListItem>
+          <Card title="Testing Card " 
+             style={{ background: "#6c757d", width: 200 , height:'100%', margin: 8 }}
+            >
+            <div>
+              <table>
+                <tbody>
+                  <tr>
+                    <td> {item.name + " " } </td>
+                    <td> {item.name + " "} </td>
+                    <td> {item.name + " " } </td>
+                  </tr>
+                  <tr>
+                    <td> {item.name + " " } </td>
+                    <td> {item.name + " "} </td>
+                    <td> {item.name + " " } </td>
+                  </tr>
+                  <tr>
+                    <td> {item.name + " " } </td>
+                    <td> {item.name + " "} </td>
+                    <td> {item.name + " " } </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </Card>
+          <div>
+            <ListItem
+              style={{ background: "yellow", width: 200 , height: 40, margin: 8 }}
+            >
+              <ListItemText
+              primary = {new Date().toDateString()+count}
+                
+                // primary={moment().format("MMM Do YY")+""}
+                // secondary={this.ShowCurrentDate}
+              />
+              
+            </ListItem>
+          </div>
         </div>
       );
     });
